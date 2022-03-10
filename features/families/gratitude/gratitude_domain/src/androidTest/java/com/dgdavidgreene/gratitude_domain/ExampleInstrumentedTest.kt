94cec2d9f39fd734e6a74f337f104b77c3d5b9ac
David Greene <dgdavidgreene@yahoo.com>
2022-03-09 20:54:47 -0800

    Initial feature - Gratitude

--- features/families/gratitude/gratitude_domain/src/androidTest/java/com/dgdavidgreene/gratitude_domain/ExampleInstrumentedTest.kt
+++ features/families/gratitude/gratitude_domain/src/androidTest/java/com/dgdavidgreene/gratitude_domain/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.dgdavidgreene.ui
+package com.dgdavidgreene.gratitude_domain
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dgdavidgreene.ui.test", appContext.packageName)
+        assertEquals("com.dgdavidgreene.gratitude_domain.test", appContext.packageName)
     }
 }
